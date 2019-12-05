7e3f4d27ccf6d7b61e2c3039bc84ab4669645b69
Vladislav Novak <novak.vlad97@mail.ru>
2019-12-05 22:48:01 +0300

    Изменения страницы планировщика.

--- ApartmentPlanner/Scripts/DeveloperScripts/CanvasPlannerScript.js
+++ ApartmentPlanner/Scripts/DeveloperScripts/CanvasPlannerScript.js
@@ -9,7 +9,8 @@ var context = canvas.getContext("2d");
 context.strokeStyle = '#001EFF';
 
 var Header = document.getElementById("header");
-var added_elems = document.getElementById("added_elems");
+var addedElems = document.getElementById("added_elems");
+var addedElemsContainer = document.getElementById("added_elems_container");
 
 var mouse = {
     x: 0,
@@ -43,14 +44,157 @@ Rect.prototype = {
     }
 }
 
-var i, rects = [];
+var i, rects = [], count = 0, strokeRect = false;
+
+
 
 function add(imgSrc) {
-    rects.push(new Rect(imgSrc, 100, 100, 100, 107));
+    rects.push(new Rect(imgSrc, 400, 300, 100, 107));
+
+    let item = document.createElement("div");
+    item.setAttribute("class", "added_elem");
+    item.setAttribute("id", "item" + count);
+
+    let test = count;
+    item.onmouseover = () => { strokeRect = rects[test]; }
+    item.onmouseout = () => { strokeRect = false }
+
+    let img = document.createElement("img");
+    img.setAttribute("src", imgSrc);
+    img.setAttribute("class", "img_added_elem");
+
+    count++;
+    let textField = document.createElement("input");
+    textField.setAttribute("value", "Item " + count);
+    textField.setAttribute("class", "textfield_added_elem");
+
+    let burger_params = document.createElement("div");
+    burger_params.setAttribute("class", "burger_params");
+    burger_params.setAttribute("onclick", "addParams(" + (count-1) + ")");
+    let burger_params_line = document.createElement("div");
+    burger_params_line.setAttribute("class", "burger_params_line");
+    burger_params.appendChild(burger_params_line);
+
+    item.appendChild(img);
+    item.appendChild(textField);
+    item.appendChild(burger_params);
+    addedElemsContainer.appendChild(item);
 }
 
-function clearr() {
+function addParams(id) {
+    var item = document.getElementById("item" + id);
+
+    if (item.querySelector("div.params_box")) {
+        let findParamsBox = item.lastChild;
+        item.removeChild(findParamsBox);
+
+        let burgerParams = item.querySelector("div.burger_to_x");
+        burgerParams.removeAttribute("class");
+        burgerParams.setAttribute("class", "burger_params_line");
+    } else {
+        let paramsBox = document.createElement("div");
+        paramsBox.setAttribute("class", "params_box");
+        paramsBox.setAttribute("style", "width: 100%; height: 80px; display: flex; flex-direction: row-reverse;");
+
+        //поля с коориднатами
+        let XYBox = document.createElement("div");
+        XYBox.setAttribute("class", "XYBox");
+
+        let labelAndInputX = document.createElement("div");
+        let labelX = document.createElement("label");
+        labelX.innerText = "X: ";
+        let textFieldX = document.createElement("input");
+        textFieldX.setAttribute("class", "text_field_params_box");
+        textFieldX.setAttribute("value", rects[id].x);
+        textFieldX.setAttribute("id", "inputx" + id);
+        textFieldX.setAttribute("oninput", "inputX(" + id + ")");
+        labelAndInputX.appendChild(labelX);
+        labelAndInputX.appendChild(textFieldX);
+
+        let labelAndInputY = document.createElement("div");
+        let labelY = document.createElement("label");
+        labelY.innerText = "Y: ";
+        let textFieldY = document.createElement("input");
+        textFieldY.setAttribute("class", "text_field_params_box");
+        textFieldY.setAttribute("value", rects[id].y);
+        textFieldY.setAttribute("id", "inputy" + id);
+        textFieldY.setAttribute("oninput", "inputY(" + id + ")");
+        labelAndInputY.appendChild(labelY);
+        labelAndInputY.appendChild(textFieldY);
+
+        XYBox.appendChild(labelAndInputX);
+        XYBox.appendChild(labelAndInputY);
+
+        //поля с шириной и высотой
+        let WHBox = document.createElement("div");
+        WHBox.setAttribute("class", "XYBox");
+
+        let labelAndInputW = document.createElement("div");
+        let labelW = document.createElement("label");
+        labelW.innerText = "W: ";
+        let textFieldW = document.createElement("input");
+        textFieldW.setAttribute("class", "text_field_params_box");
+        textFieldW.setAttribute("value", rects[id].w);
+        textFieldW.setAttribute("id", "inputw" + id);
+        textFieldW.setAttribute("oninput", "inputW(" + id + ")");
+        labelAndInputW.appendChild(labelW);
+        labelAndInputW.appendChild(textFieldW);
+
+        let labelAndInputH = document.createElement("div");
+        let labelH = document.createElement("label");
+        labelH.innerText = "H: ";
+        let textFieldH = document.createElement("input");
+        textFieldH.setAttribute("class", "text_field_params_box");
+        textFieldH.setAttribute("value", rects[id].h);
+        textFieldH.setAttribute("id", "inputh" + id);
+        textFieldH.setAttribute("oninput", "inputH(" + id + ")");
+        labelAndInputH.appendChild(labelH);
+        labelAndInputH.appendChild(textFieldH);
+
+        WHBox.appendChild(labelAndInputW);
+        WHBox.appendChild(labelAndInputH);
+
+        paramsBox.appendChild(WHBox);
+        paramsBox.appendChild(XYBox);
+        item.appendChild(paramsBox);
+
+        let burgerParams = item.querySelector("div.burger_params_line");
+        burgerParams.removeAttribute("class");
+        burgerParams.setAttribute("class", "burger_to_x");
+    }
+}
+
+//ввод x
+function inputX(id) {
+    let textFieldX = document.getElementById("inputx" + id);
+    rects[id].x = parseInt(textFieldX.value);
+}
+
+//ввод y
+function inputY(id) {
+    let textFieldY = document.getElementById("inputy" + id);
+    rects[id].y = parseInt(textFieldY.value);
+}
+
+//ввод w
+function inputW(id) {
+    let textFieldW = document.getElementById("inputw" + id);
+    rects[id].w = parseInt(textFieldW.value);
+}
+
+//ввод h
+function inputH(id) {
+    let textFieldH = document.getElementById("inputh" + id);
+    rects[id].h = parseInt(textFieldH.value);
+}
+
+function clearContainer() {
     rects.splice(0);
+    while (addedElemsContainer.firstChild != null) {
+        count--;
+        let elem = addedElemsContainer.firstChild;
+        addedElemsContainer.removeChild(elem);
+    }
 }
 
 var isCursorInRect = (rect) => {
@@ -90,11 +234,34 @@ setInterval(() => {
     canvas.style.cursor = "default";
 
     for (i in rects) {
-        rects[i].draw();
+        //отрисовка объектов
+        //if (i == 1) {
+        //    context.save();
+        //    context.rotate(Math.PI / 2);
+        //    rects[i].draw();
+        //    rects[i].x = 300;
+        //    rects[i].y = -400;
+        //    context.restore();
+        //} else {
+            rects[i].draw();
+        //}
+
+        let item = document.getElementById("item" + i);
+        //сброс фона добавленного элемента
+        item.setAttribute("style", "");
 
         if (isCursorInRect(rects[i])) {
+            //отрисовка синих границ объекта
             rects[i].stroke();
+
+            //установка фона выбранного добавленного элемента
+            item.setAttribute("style", "background-color: rgb(219, 226, 226);");
         }
+    }
+
+    //отрисовка синих границ объекта, если навелись на item
+    if (strokeRect != false) {
+        strokeRect.stroke();
     }
 
     //перемещение объекта
@@ -109,7 +276,7 @@ setInterval(() => {
     }
 
     //не применять если высота или ширина слишком маленькие
-    if (selected.h >= 40 && selected.w >= 40) {
+    if (selected && selected.h >= 40 && selected.w >= 40) {
         //изменение размера объекта
         //влево
         if (changeSizeXLeft) {
@@ -161,8 +328,26 @@ setInterval(() => {
 }, 1);
 
 canvas.onmousemove = function (e) {
-    mouse.x = e.pageX - added_elems.offsetWidth;
+    mouse.x = e.pageX - addedElems.offsetWidth;
     mouse.y = e.pageY - Header.offsetHeight;
+
+    for (i in rects) {
+        //запись координат в поля x и y
+        let textFieldX = document.getElementById("inputx" + i);
+        let textFieldY = document.getElementById("inputy" + i);
+        if (textFieldX != null || textFieldY != null) {
+            textFieldX.value = rects[i].x;
+            textFieldY.value = rects[i].y;
+        }
+
+        //запись параметров в поля w и h
+        let textFieldW = document.getElementById("inputw" + i);
+        let textFieldH = document.getElementById("inputh" + i);
+        if (textFieldW != null || textFieldH != null) {
+            textFieldW.value = rects[i].w;
+            textFieldH.value = rects[i].h;
+        }
+    }
 
     if (selected) {
         if (mouse.x - selected.x < 10) {
